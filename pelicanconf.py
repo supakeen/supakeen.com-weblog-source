d61f32c5776d52de522d4cf611e153e4b99dc8df
supakeen <cmdr@supakeen.com>
2020-01-12 20:38:40 +0000

    Correct path.

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,4 +34,4 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "../supakeen.github.io-weblog-theme"
+THEME = "../supakeen.com-weblog-theme"
